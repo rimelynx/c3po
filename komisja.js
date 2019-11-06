5a739130de4f001a5ee20fe8a2c5a510a73de5f8
Azzie <azzie@astronet.pl>
2019-11-06 02:30:14 +0100

    New version of the app that supports voting for transmission.

--- komisja.js
+++ komisja.js
@@ -1,19 +1,63 @@
-Promise.all([dom.contentLoaded, crypto.generateKeyPair()]).then(start);
+dom.contentLoaded.then(start);
 
 function start(args) {
-  let keyPair = args[1];
-  let votes = new Votes();
+  let privateKey = null;
+  let votes = null;
 
-  crypto.exportPublicKey(keyPair.publicKey).then(publicKeyData => {
-    dom.setValue("public_key", buffer.toBase64(publicKeyData));
-  });
-  dom.addClickListener("add", addVote);
-  dom.addClickListener("sum", summarizeVotes);
+  dom.addClickListener("more", addOption);
+  dom.addClickListener("create", createPoll);
+
+  function addOption() {
+    let option = document.createElement("input");
+    option.name = "option";
+    let button = document.getElementById("more");
+    dom.prependSibling(button, option);
+    dom.prependSibling(button, " ");
+  }
+
+  function createPoll() {
+    forgetKey();
+    clearVotes();
+
+    let poll = {
+      options: []
+    };
+    let options = document.getElementsByName("option");
+    for (let option of options) {
+      if (option.value) {
+        poll.options.push(option.value);
+      }
+    }
+    if (!poll.options.length) {
+      return;
+    }
+    poll.multi = document.getElementsByName("multi")[0].checked;
+    let pollData = buffer.fromString(JSON.stringify(poll));
+
+    crypto.generateKeyPair().then(keyPair => {
+      privateKey = keyPair.privateKey;
+      return crypto.exportPublicKey(keyPair.publicKey);
+    }).then(publicKeyData => {
+      dom.setValue("key", buffer.toBase64(pollData) + "_" +
+                          buffer.toBase64(publicKeyData));
+      dom.addClickListener("add", addVote);
+      dom.addClickListener("sum", summarizeVotes);
+    });
+  }
 
   function addVote() {
-    let ciphertext = buffer.fromBase64(dom.getValue("ballot"));
-    crypto.decrypt(keyPair.privateKey, ciphertext).then(plaintext => {
-      votes.add(buffer.toString(plaintext));
+    let ballot = dom.getValue("ballot");
+    if (!ballot) {
+      return;
+    }
+    let ciphertext = buffer.fromBase64(ballot);
+    crypto.decrypt(privateKey, ciphertext).then(plaintext => {
+      let vote = JSON.parse(buffer.toString(plaintext));
+      if (document.getElementsByName("multi")[0].checked) {
+        votes.addMultiple(vote);
+      } else {
+        votes.addOne(vote[0] || "-");
+      }
       document.getElementById("count").textContent = votes.total;
       // Clear the ballot after reading to avoid repeated additions.
       dom.clearValue("ballot");
@@ -21,9 +65,23 @@ function start(args) {
   }
 
   function summarizeVotes() {
-    // Forget the private key when producing a summary.
-    keyPair.privateKey = null;
-    dom.clearValue("public_key");
+    forgetKey();
     dom.setValue("summary", votes.summarize());
+  }
+
+  function forgetKey() {
+    if (!privateKey) {
+      return;
+    }
+    dom.removeListeners("sum");
+    dom.removeListeners("add");
+    privateKey = null;
+    dom.clearValue("key");
+  }
+
+  function clearVotes() {
+    votes = new Votes();
+    dom.clearValue("summary");
+    document.getElementById("count").textContent = 0;
   }
 }
--- wyborca.js
+++ wyborca.js
@@ -1,14 +1,57 @@
 dom.contentLoaded.then(start);
 
 function start() {
-  dom.addClickListener("encrypt", encryptVote);
+  let publicKey = null;
+
+  dom.addClickListener("show", showForm);
+
+  function showForm() {
+    dom.clearValue("ballot");
+
+    let parts = dom.getValue("key").split("_");
+    if (parts.length != 2) {
+      return;
+    }
+
+    let poll = JSON.parse(buffer.toString(buffer.fromBase64(parts[0])));
+    let form = document.getElementById("vote");
+    let button = document.getElementById("encrypt").cloneNode(true);
+    while (form.lastChild) {
+      form.removeChild(form.lastChild);
+    }
+    let n = 1;
+    for (let option of poll.options) {
+      let input = document.createElement("input");
+      input.type = poll.multi ? "checkbox" : "radio";
+      input.name = "option";
+      input.id = "option" + n++;
+      input.value = option;
+      let label = document.createElement("label");
+      label.htmlFor = input.id;
+      label.textContent = option;
+      dom.appendChild(form, input);
+      dom.appendChild(form, " ");
+      dom.appendChild(form, label);
+      dom.appendChild(form, " ");
+    }
+    dom.appendChild(form, button);
+
+    crypto.importPublicKey(buffer.fromBase64(parts[1])).then(key => {
+      publicKey = key;
+      dom.addClickListener("encrypt", encryptVote);
+    });
+  }
 
   function encryptVote() {
-    let publicKeyData = buffer.fromBase64(dom.getValue("public_key"));
-    crypto.importPublicKey(publicKeyData).then(publicKey => {
-      var plaintext = buffer.fromString(dom.getValue("vote"));
-      return crypto.encrypt(publicKey, plaintext)
-    }).then(ciphertext => {
+    let vote = [];
+    let options = document.getElementsByName("option");
+    for (option of options) {
+      if (option.checked) {
+        vote.push(option.value);
+      }
+    }
+    var plaintext = buffer.fromString(JSON.stringify(vote));
+    return crypto.encrypt(publicKey, plaintext).then(ciphertext => {
       dom.setValue("ballot", buffer.toBase64(ciphertext));
     });
   }
